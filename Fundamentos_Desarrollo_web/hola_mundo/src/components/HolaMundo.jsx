f048649d6624ae758112cba08121d4cbbc93e6af
David Ricardo Rivera Arbeláez <davidriveraarbelaez@gmail.com>
2025-05-06 08:40:02 -0500

    Primer componente en React

---
+++ Fundamentos_Desarrollo_web/hola_mundo/src/components/HolaMundo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HolaMundo = () => {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <p>Loremsss ipsum dolor sit amet consectetur adipisicing elit. Possimus quisquam reiciendis quidem, dolorum perferendis consequatur porro ea saepe! Et est sed quod, maxime eligendi magni exercitationem fugiat ab deleniti laborum!</p>
+    </>  
+    )
+}
